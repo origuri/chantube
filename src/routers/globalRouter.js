8fce916b5e9b71eb5919d41a693ea61107837ab2
origuri <dlcksrb7@naver.com>
2023-07-02 18:13:09 +0900

    2023.7.2-라우터와 컨트롤러 분리

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
+import {tranding} from "../controllers/videoController";
+import {join} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-const handleJoin = (req, res) => res.send("Join");
-
-globalRouter.get("/", handleHome);
-globalRouter.get("/join", handleJoin);
+globalRouter.get("/", tranding);
+globalRouter.get("/join", join);
 
 // export default를 하면 globalRouter.js를 임포트하는 순간 이 파일 전체가 임포트되는 것이 아닌
 // const globalRouter = express.Router(); 를 임포트하는 것.
