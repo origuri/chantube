78bec251fdc51a1d49a2a5799cf131ed965fb28b
origuri <dlcksrb7@naver.com>
2023-07-02 17:40:20 +0900

    2023.7.2-서버와 라우터 분리

--- src/server.js
+++ src/server.js
@@ -1,35 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 // express로 앱을 만들기 위한 첫번째 약속 변수 명은 app이라고 할 것 
 const app = express();
+const logger = morgan("dev");
+app.use(logger)
 
-// 앱이 실행되고 listen하는 사이에 여러가지 명렁어들을 넣을 것
-// express는 eventHandler의 event 아규먼트처럼 callback함수에 공짜로 request와 response를 제공해줌
-const logger = (req, res, next) =>{
-    console.log(`${req.method} ${req.url}`)
-    next();
-}
 
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>접근금지!!</h1>")
-    } else{
-        next();
-    }
-}
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleHome = (req, res, next) => {
-    return res.end();
-}
 
-const handleprotected = (req, res)=>{
-    return res.send("this is private!");
-}
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/",handleHome);
-app.get("/protected", handleprotected)
 
 //포트를 선언함으로써 변수로 사용해서 백틱으로 잡게 할거임
 const PORT = 4000;
